832625418e938ec140ef7733d918420541218449
Martin Farkaš <martin.farkas@unicorn.com>
2021-06-23 09:17:24 +0200

    Turn off rule react/jsx-no-bind

--- uu_jokesg01-core/.eslintrc.js
+++ uu_jokesg01-core/.eslintrc.js
@@ -1,12 +1,16 @@
 const path = require("path");
 
+// TODO Sent request to uuAppDevkit to extend default configuration
 let config = {
   extends: ["./" + path.relative(".", require.resolve("uu_appg01_devkit/src/config/.eslintrc.js")).replace(/\\/g, "/")],
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  rules: {
+    "react/jsx-no-bind": 0,
+  },
 };
-
-// TODO Sent request to uuAppDevkit to extend default configuration
-config.settings = {};
-config.settings.react = {};
-config.settings.react.version = "detect";
 
 module.exports = config;
