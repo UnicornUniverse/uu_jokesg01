17f04c7b964b21b614f8f26c1283b8df7bd50fd4
Martin Farkaš <martin.farkas@unicorn.com>
2021-08-16 12:54:52 +0200

    Add ISSUE URLs to ESLint configuration

--- uu_jokesg01-core/.eslintrc.js
+++ uu_jokesg01-core/.eslintrc.js
@@ -1,6 +1,10 @@
 const path = require("path");
 
-// TODO Sent request to uuAppDevkit to extend default configuration
+// ISSUE uuAppDevkit ESLint configuration doesn't contain settings.react.version
+// https://uuapp.plus4u.net/uu-sls-maing01/f34e4c65a9c84ea0baf50017c43fc97d/issueDetail?id=611a32a5545e5300294d52a6
+
+// ISSUE uuAppDevkit should turn off the ESLint rule react/jsx-no-bind
+// https://uuapp.plus4u.net/uu-sls-maing01/f34e4c65a9c84ea0baf50017c43fc97d/issueDetail?id=611a33eb545e5300294d5318
 let config = {
   extends: ["./" + path.relative(".", require.resolve("uu_appg01_devkit/src/config/.eslintrc.js")).replace(/\\/g, "/")],
   settings: {
