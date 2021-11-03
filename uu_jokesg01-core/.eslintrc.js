78653e5131c46162b906d3a8e29a9716c4fbfbbc
Martin Farkaš <martin.farkas@unicorn.com>
2021-11-03 11:20:35 +0100

    Remove hotfix of ESLint configuration

--- uu_jokesg01-core/.eslintrc.js
+++ uu_jokesg01-core/.eslintrc.js
@@ -1,20 +1,7 @@
 const path = require("path");
 
-// ISSUE uuAppDevkit ESLint configuration doesn't contain settings.react.version
-// https://uuapp.plus4u.net/uu-sls-maing01/f34e4c65a9c84ea0baf50017c43fc97d/issueDetail?id=611a32a5545e5300294d52a6
-
-// ISSUE uuAppDevkit should turn off the ESLint rule react/jsx-no-bind
-// https://uuapp.plus4u.net/uu-sls-maing01/f34e4c65a9c84ea0baf50017c43fc97d/issueDetail?id=611a33eb545e5300294d5318
 let config = {
   extends: ["./" + path.relative(".", require.resolve("uu_appg01_devkit/src/config/.eslintrc.js")).replace(/\\/g, "/")],
-  settings: {
-    react: {
-      version: "detect",
-    },
-  },
-  rules: {
-    "react/jsx-no-bind": 0,
-  },
 };
 
 module.exports = config;
