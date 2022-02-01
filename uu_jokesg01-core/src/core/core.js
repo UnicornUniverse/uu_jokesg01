84b0a6a289741f10896b09467d62a07228b05313
Martin Farkaš <martin.farkas@unicorn.com>
2022-02-01 11:05:27 +0100

    Hook useTraceUpdate moved to uu5g05

--- uu_jokesg01-core/src/core/core.js
+++ uu_jokesg01-core/src/core/core.js
@@ -3,5 +3,4 @@ export * from "./error.js";
 export * from "./error-boundary.js";
 export * from "./data-object-state-resolver.js";
 export * from "./data-list-state-resolver.js";
-export * from "./use-trace-update.js";
 export * from "./prevent-leave-controller.js";
