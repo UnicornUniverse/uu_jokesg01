90d224ad679902b0c283fecc811476013aa80cce
Martin Farkaš <martin.farkas@unicorn.com>
2022-08-16 10:41:51 +0200

    Fix ESLint path

--- uu_jokesg01/.eslintrc.js
+++ uu_jokesg01/.eslintrc.js
@@ -2,15 +2,7 @@ const path = require("path");
 
 // TODO Sent request to uuAppDevkit to extend default configuration
 let config = {
-  extends: ["./" + path.relative(".", require.resolve("uu_appg01_devkit/src/config/.eslintrc.js")).replace(/\\/g, "/")],
-  settings: {
-    react: {
-      version: "detect",
-    },
-  },
-  rules: {
-    "react/jsx-no-bind": 0,
-  },
+  extends: [path.relative(".", require.resolve("uu_appg01_devkit/src/config/.eslintrc.js")).replace(/\\/g, "/")],
 };
 
 module.exports = config;
